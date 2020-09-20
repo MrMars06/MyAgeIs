4808d35bcfa7c3415702398173ba23d9b492ee1a
Marcelo Domhoff <marcelo.domhoff@patelhs.org>
2020-09-20 15:30:58 +0000

    Added if else statement for myAge.

--- Main.java
+++ Main.java
@@ -2,7 +2,13 @@ class CheckMyAge {
   public static void main(String[] args) {
     int myAge = 14;
     int myBirthday = 21/7;
-    int todaysDate = 09/21;
+    int todaysDate = 9/21;
   
+  if(myBirthday == todaysDate)
+  {
+    System.out.println("I am " + myAge + "years old.");
+  }else{
+    System.out.println("I am still " + myAge + "years old.");
+  }
   }
 }

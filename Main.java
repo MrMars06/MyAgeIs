18ac8a776f5e5a5211b3056299192a6d605e40a7
Marcelo Domhoff <marcelo.domhoff@patelhs.org>
2020-09-20 15:17:44 +0000

    Almost finished with first if else statement

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 class CheckMyAge {
   public static void main(String[] args) {
+    int age = 14;
+
+    {if(age == 15);
     
+      System.out.println("You are 14 years old.");
+    }else{
+      System.out.println("You are not 14 years old.");
+    }
   }
 }
